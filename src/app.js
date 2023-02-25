087cfca5fc025df7671b02b662ed3b45491ded9a
jgrant928 <jsgrant928@gmail.com>
2023-02-25 16:06:41 -0500

    added app.js

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+import ContactForm from './components/Contact';
+import Resume from './components/Resume';
+
+function App() {
+  const categories = ["projects", "photography"];
+  const [contactSelected, setContactSelected] = useState(false);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [resumeSelected, setResumeSelected] = useState(false);
+  const [aboutSelected, setAboutSelected] = useState(false);
+  
+  return (
+    <div>
+      <Header 
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+        resumeSelected={resumeSelected}
+        setResumeSelected={setResumeSelected}
+        aboutSelected={aboutSelected}
+        setAboutSelected={setAboutSelected}
+        ></Header>
+      <main>
+        <div>
+        {!contactSelected && !resumeSelected && !aboutSelected ? (
+          <>
+            <Portfolio currentCategory={currentCategory}></Portfolio>
+          </>
+        ) : !contactSelected && !aboutSelected ? (
+          <Resume></Resume>
+        ): !aboutSelected ? (
+          <ContactForm></ContactForm>
+        ): (
+        <About></About>)}
+        </div>
+      </main>
+        <Footer></Footer>
+
+    </div>
+  );
+}
+
+export default App;
